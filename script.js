b4061c2c7a4090ec21e1708c769f8bd464c57cdf
henriquemarley <57380430+henriquemarley@users.noreply.github.com>
2020-09-10 22:57:49 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-document.getElementById("add").onclick = function(){
-    console.log('Hello world');
-    }
+document.getElementById("add").onclick  = function() {
+
+    var li = document.createElement("Li");
+    var texto = document.getElementById("hobbieUsuario").value; 
+    var textnode = document.createTextNode(texto);
+    li.appendChild(textnode);
+    document.getElementById("listaHobbies").appendChild(li);
+}
+
+document.getElementById("add2").onclick  = function() {
+
+    var li = document.createElement("Li");
+    var texto = document.getElementById("perguntaUsuario").value; 
+    var textnode = document.createTextNode(texto);
+    li.appendChild(textnode);
+    document.getElementById("listaPerguntas").appendChild(li);
+}
