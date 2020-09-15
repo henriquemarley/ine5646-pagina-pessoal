6bcf55a3d0a2bb2589c1eb3309eb2d18881e3f38
henriquemarley <57380430+henriquemarley@users.noreply.github.com>
2020-09-14 23:13:08 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -15,3 +15,22 @@ document.getElementById("add2").onclick  = function() {
     li.appendChild(textonode);
     document.getElementById("listaPerguntas").appendChild(li);
 }
+
+document.getElementById("alterarCurso").onclick = function() {
+    var nome;
+    nome = prompt("Digite o seu novo cargo ou curso");
+    if(nome != "" && nome != null) {
+    document.getElementById("curso").innerText = nome; 
+    } else {
+        alert("É necessário digitar um curso válido.")
+    }
+}    
+document.getElementById("alterarCargo").onclick = function() {
+    var nome;
+    nome = prompt("Digite o seu novo cargo");
+    if(nome != "" && nome != null) {
+    document.getElementById("cargo").innerText = nome; 
+    } else {
+        alert("É necessário digitar uma profissão válida.")
+    }
+}    
